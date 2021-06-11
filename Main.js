00f5cd68e1c83f7e7bd718c6c4dbe7a18eb91ee9
xxDIERxx <reidschrein@gmail.com>
2021-06-11 16:26:36 -0400

    Updated

--- Main.js
+++ Main.js
@@ -1,14 +1,5 @@
-$(document).ready(function(){
+$(function (){
 
-    fs.readFile('Interview/Documents.csv' ,'urf8', (err, data) =>{
-        if (err){
-            console.error(err)
-            return
-        }
-        console.log(data)
-    })
+
+
 });
-
-
-
-
--- index.js
+++ index.js
@@ -3,20 +3,30 @@ const readline = require('readline');
 const path = require('path');
 const express = require("express");
 const flash = require('express-flash');
-const session = require('express-session');
-
+const csv = require('csvtojson');
 
 const app = express();
-
 app.use(flash());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}));
 
-const port = process.env.PORT || 8080;
-const server = app.listen(port, () => {             //create server and start listening
-    const {address, port} = server.address();
-    console.log(`listening on ${address}:${port}`);
+
+
+const server = app.listen(8080, function () {             //create server and start listening
+    var host = server.address().address
+    var port = server.address().port
+    console.log(`listening on http:/%s:%s/`, host, port);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Main.html'));
+});
+
+
+
+const csvfilePath = path.join(__dirname, '/Interview/Documents.csv');
+console.log(csvfilePath);
+csv()
+.fromFile(csvfilePath)
+.then((jsonObj)=>{
+    const jsonArray = jsonObj;
+    console.log(jsonArray);
+})
