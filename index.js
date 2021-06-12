4ba0972592025a6fcb10ad89ddd81ef35fb2b7f3
xxDIERxx <reidschrein@gmail.com>
2021-06-12 17:49:58 -0400

    Working JSON file.
    CSV file is converted properly to JSON file. Now need to apply the data to the table on the page and apply CRUD operations

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ const server = app.listen(8080, function () {             //create server and st
     var port = server.address().port
     console.log(`listening on http:/%s:%s/`, host, port);
 });
+const csvfilePath = path.join(__dirname, '/Interview/Documents.csv');
+
+myFile = fs.readFileSync(csvfilePath)
+var array = myFile.toString().split("\r");
+console.log(array);
+let result = [];
+let headers = array[0].split(",");
+console.log(headers);
+
+for (let i = 1; i < array.length - 1; i++) {
+  var obj = {};
+  var currentLine = array[i].split(",");
+  console.log(currentLine);
+  for(var j=0;j<headers.length;j++){
+    obj[headers[j]] = currentLine[j];
+  }
+  result.push(obj);
+
+}
+   console.log(result);
+// Convert the resultant array to json and
+// generate the JSON output file.
+let json = JSON.stringify(result);
+fs.writeFileSync('output.json', json);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/Main.html'));
@@ -22,11 +46,3 @@ app.get('/', (req, res) => {
 
 
 
-const csvfilePath = path.join(__dirname, '/Interview/Documents.csv');
-console.log(csvfilePath);
-csv()
-.fromFile(csvfilePath)
-.then((jsonObj)=>{
-    const jsonArray = jsonObj;
-    console.log(jsonArray);
-})
