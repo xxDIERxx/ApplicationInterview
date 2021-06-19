eb7669c82f6de101760b74108ac5733c9fe359c9
xxDIERxx <reidschrein@gmail.com>
2021-06-18 21:28:18 -0400

    Updated Main
    Links are now functioning for items in the csv file

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const express = require("express");
 const flash = require('express-flash');
 const csv = require('csvtojson');
-const { request } = require('http');
 
 const app = express();
 app.use(flash());
@@ -14,14 +13,14 @@ app.use(flash());
 const server = app.listen(8080, function () {             //create server and start listening
     var host = server.address().address
     var port = server.address().port
-    console.log(`listening on http:/%s:%s/`, host, port);
+    console.log(`listening on http:/%s/`, host, port);
 });
 
 const csvfilePath = path.join(__dirname, '/Interview/Documents.csv');
 
 myFile = fs.readFileSync(csvfilePath)
 var array = myFile.toString().split("\r");
-console.log(array);
+//console.log(array);
 let result = [];
 let headers = array[0].split(",");
 
@@ -32,24 +31,28 @@ for (let i = 1; i < array.length - 1; i++) {
     obj[headers[j]] = currentLine[j];
   }
   result.push(obj);
-
 }
-   //console.log(result);
+
+//console.log(result);
 // Convert the resultant array to json and
 // generate the JSON output file.
-/*
+
 let json = JSON.stringify(result);
 fs.writeFileSync('output.json', json);
-console.log(json);
-*/
+//console.log(json);
+
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Main.html'));
+  res.sendFile(path.join(__dirname, '/Main.html'));
 });
+
 
 app.get('/Interview/Documents.csv',(req, res) =>{
   res.sendFile(csvfilePath);
 });
 
-
+//Whenever the Link is pressed, Dynamically respond from the server the correct File.
+app.get('/Docs/:name/:id', function(req, res) {
+  res.sendFile(path.join(__dirname, '/Interview/Docs/' + req.params.name +"/" + req.params.id));
+});
